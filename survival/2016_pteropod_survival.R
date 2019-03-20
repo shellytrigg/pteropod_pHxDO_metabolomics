ec2a77917ab06085aed0a7ab21e0154ca8f7aa18
shelly trigg <shellywanamaker@gmail.com>
2019-03-20 00:58:27 -0700

    r script

--- survival/2016_pteropod_survival.R
+++ survival/2016_pteropod_survival.R
@@ -1,6 +1,7 @@
 #load libraries
 library(readxl)
 library(tidyr)
+library(ggplot2)
 
 d <- read_xlsx("~/Documents/GitHub/pteropod_pHxDO_metabolomics/survival/pteropod_pHxDO2016_masterdatasheet.xlsx", sheet = "sample IDs living", skip = 2)
 weird_date <- as.numeric(colnames(d)[3:11], quote = FALSE)
@@ -17,4 +18,25 @@ STACKED_d <- gather(d, "Date", "state", 3:11)
 
 #how many states are there?
 unique(STACKED_d$state)
+
+for(i in 1:length(STACKED_d$state)){
+  if(STACKED_d$state[i] == "D"){
+    STACKED_d$state_num[i] <- 0
+  }
+  if(STACKED_d$state[i] == "M"){
+    STACKED_d$state_num[i] <- 0.5
+  }
+  if(STACKED_d$state[i] == "L"){
+    STACKED_d$state_num[i] <- 1
+  }
+  if(STACKED_d$state[i] == "2L"){
+    STACKED_d$state_num[i] <- 2
+  }
+  if(STACKED_d$state[i]== "LL"){
+    STACKED_d$state_num[i] <- 0.75
+  }
+}
+
+STACKED_d$Date <- as.POSIXct(STACKED_d$Date)
+ggplot(STACKED_d, aes(Date, state_num)) + geom_line() + facet_wrap(~MOATS)
 
